1f45a88de876658cbdc1b5312a3eda1acf92204f
GreedyOne <117036296+GreedyOn3@users.noreply.github.com>
2025-08-10 18:38:01 +0200

    Refactored TtlDictionary.cs
    -Refactored TtlDictionary.cs for thread-safe concurrent usage.
    -Added GetOrAddAsync()

--- FivetranClient/Infrastructure/TtlDictionary.cs
+++ FivetranClient/Infrastructure/TtlDictionary.cs
@@ -1,35 +1,80 @@
-﻿namespace FivetranClient.Infrastructure;
+using System.Collections.Concurrent;
+using System.Threading;
 
-public class TtlDictionary<TKey, TValue> where TKey : notnull
+namespace FivetranClient.Infrastructure;
+
+public class TtlDictionary<TKey, TValue> where TKey : notnull 
 {
-    private readonly Dictionary<TKey, (TValue, DateTime)> _dictionary = new();
+    private readonly ConcurrentDictionary<TKey, (TValue Value, DateTime Expiration)> _dictionary = new();
+    private readonly ConcurrentDictionary<TKey, SemaphoreSlim> _locks = new();
 
-    public TValue GetOrAdd(TKey key, Func<TValue> valueFactory, TimeSpan ttl)
+    private SemaphoreSlim GetLock( TKey key ) =>
+        _locks.GetOrAdd( key, _ => new SemaphoreSlim( 1, 1 ) );
+
+
+    public TValue GetOrAdd(TKey key, Func<TValue> valueFactory, TimeSpan ttl) 
     {
-        if (_dictionary.TryGetValue(key, out var entry))
-        {
-            if (DateTime.UtcNow < entry.Item2)
-            {
-                return entry.Item1;
-            }
+        if( TryGetValidValue(key, out var cachedValue))
+            return cachedValue;
 
-            _dictionary.Remove(key);
+        var semaphore = GetLock(key);
+        semaphore.Wait();
+        try {
+            if( TryGetValidValue(key, out cachedValue))
+                return cachedValue;
+
+            var value = valueFactory();
+            _dictionary[key] = (value, DateTime.UtcNow.Add(ttl));
+            return value;
+        } finally {
+            semaphore.Release();
         }
-
-        var value = valueFactory();
-        _dictionary[key] = (value, DateTime.UtcNow.Add(ttl));
-        return value;
     }
 
-    public bool TryGetValue(TKey key, out TValue value)
+    public async Task<TValue> GetOrAddAsync(TKey key, Func<Task<TValue>> asyncFactory, TimeSpan ttl) 
     {
-        if (_dictionary.TryGetValue(key, out var entry) && DateTime.UtcNow < entry.Item2)
-        {
-            value = entry.Item1;
-            return true;
+        if( TryGetValidValue(key, out var cachedValue))
+            return cachedValue;
+
+        var semaphore = GetLock(key);
+        await semaphore.WaitAsync();
+        try {
+            if( TryGetValidValue(key, out cachedValue))
+                return cachedValue;
+
+            var value = await asyncFactory();
+            _dictionary[key] = (value, DateTime.UtcNow.Add(ttl));
+            return value;
+        } finally {
+            semaphore.Release();
+        }
+    }
+
+
+    public bool TryGetValidValue(TKey key, out TValue value) 
+    {
+          if( _dictionary.TryGetValue(key, out var entry)) {
+            if(DateTime.UtcNow < entry.Expiration) {
+                value = entry.Value;
+                return true;
+            } else {
+                RemoveEntry(key);
+            }
         }
 
         value = default!;
         return false;
+    }
+
+    public bool RemoveEntry(TKey key) 
+    {
+        var removed = _dictionary.TryRemove(key, out _);
+
+        if(_locks.TryRemove(key, out var semaphore)) 
+        {
+            semaphore.Dispose();
+        }
+
+        return removed;
     }
 }
