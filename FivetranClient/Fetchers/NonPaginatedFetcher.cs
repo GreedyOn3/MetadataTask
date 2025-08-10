15e494b82d5aef4dbd6bf626f75dc8f39a65bb65
GreedyOne <117036296+GreedyOn3@users.noreply.github.com>
2025-08-10 18:53:29 +0200

    Update NonPaginatedFetcher.cs
    -Added try/catch block around JSON deserialization to handle potential errors.
    -(SUGGESTION) Implement logging to capture deserialization issues for easier troubleshooting.

--- FivetranClient/Fetchers/NonPaginatedFetcher.cs
+++ FivetranClient/Fetchers/NonPaginatedFetcher.cs
@@ -1,15 +1,23 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 using FivetranClient.Models;
 
 namespace FivetranClient.Fetchers;
 
-public sealed class NonPaginatedFetcher(HttpRequestHandler requestHandler) : BaseFetcher(requestHandler)
+public sealed class NonPaginatedFetcher(HttpRequestHandler requestHandler) : BaseFetcher(requestHandler) 
 {
-    public async Task<T?> FetchAsync<T>(string endpoint, CancellationToken cancellationToken)
+    public async Task<T?> FetchAsync<T>(string endpoint, CancellationToken cancellationToken) 
     {
-        var response = await base.RequestHandler.GetAsync(endpoint, cancellationToken);
-        var content = await response.Content.ReadAsStringAsync(cancellationToken);
-        var root = JsonSerializer.Deserialize<NonPaginatedRoot<T>>(content, SerializerOptions);
-        return root is null ? default(T) : root.Data;
+            var response = await base.RequestHandler.GetAsync(endpoint, cancellationToken);
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+        try 
+        {
+            var root = JsonSerializer.Deserialize<NonPaginatedRoot<T>>(content, SerializerOptions);
+            return root is null ? default : root.Data;
+
+        } catch(JsonException) 
+        {
+            //Add loging here
+            return default;
+        }
     }
 }
